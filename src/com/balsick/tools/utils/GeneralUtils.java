0f42596386b164e1e24cdb8330235c4eaf82647b
Balsick <balsick@gmail.com>
2015-10-11 16:04:37 +0200

    HTTP Server (pt. 1)

--- src/com/balsick/tools/utils/GeneralUtils.java
+++ src/com/balsick/tools/utils/GeneralUtils.java
@@ -1,14 +1,16 @@
 package com.balsick.tools.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public abstract class GeneralUtils {
 	
-	public static String[] translate(String string, String key) {
+	public static List<String> translate(String string, String key) {
 		return traduce(string, key, "=", ";");
 	}
 	
-	public static String[] traduce(String string, String key, String keyValueSeparator, String valuesSeparator) {
+	public static List<String> traduce(String string, String key, String keyValueSeparator, String valuesSeparator) {
 		String str = string;
 		if (string.indexOf(key+keyValueSeparator) == 0) {
 			str = str.substring((key+keyValueSeparator).length());
@@ -17,10 +19,10 @@ public abstract class GeneralUtils {
 		if (str.contains("{") && str.contains("}"))
 			str = str.substring(str.indexOf('{')+1, str.indexOf('}'));
 		StringTokenizer tokenizer = new StringTokenizer(str, valuesSeparator);
-		String[] result = new String[tokenizer.countTokens()];
+		List<String> result = new ArrayList<>();
 		int i = 0;
 		while (tokenizer.hasMoreTokens()){
-			result[i++] = tokenizer.nextToken();
+			result.add(tokenizer.nextToken());
 		}
 		return result;
 	}
